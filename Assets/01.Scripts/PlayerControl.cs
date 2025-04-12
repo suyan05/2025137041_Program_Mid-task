82f15c6892916e9bdbd55812743e1e77543b6f77
suyan05 <cadhppr588@gmail.com>
2025-04-12 21:40:35 +0900

    04-12 중간 push

--- Assets/01.Scripts/PlayerControl.cs
+++ Assets/01.Scripts/PlayerControl.cs
@@ -9,16 +9,20 @@ public class PlayerControl : MonoBehaviour
     public float PAttackPower = 3f;
     public float PHP = 10f;
 
+    private bool isJump = true;
+    private bool isDoubleJump = false;
+    private bool isGround = true;
+
     public Transform GroundCh;
     public LayerMask GroundLay;
 
     private Rigidbody2D rb;
-    private bool isGround = true;
-    private bool isDoubleJump = true;
+    private Animator PAni;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        PAni = GetComponent<Animator>();
     }
 
     private void Update()
@@ -31,22 +35,55 @@ public class PlayerControl : MonoBehaviour
         float MoveInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(MoveInput * PSpeed, rb.velocity.y);
 
-        isGround = Physics2D.OverlapCircle(GroundCh.position, 0.2f, GroundLay);
+        isJump = Physics2D.OverlapCircle(GroundCh.position, 0.2f, GroundLay);
 
-        if(isGround&&Input.GetKeyDown(KeyCode.Space))
+        if (MoveInput > 0) 
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+            PAni.SetBool("Player_Move", true);
+        }
+        else if (MoveInput < 0) 
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            PAni.SetBool("Player_Move", true);
+        }
+        else { PAni.SetBool("Player_Move", false); }
+
+        if (isJump && Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
             isDoubleJump = true;
         }
-        else if(isDoubleJump && Input.GetKeyDown(KeyCode.Space))
+        else if (isDoubleJump && Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
             isDoubleJump = false;
         }
+        else if (isGround == true) { PAni.SetBool("Player_Jump", false); }
     }
 
     private void Jump()
     {
         rb.AddForce(Vector2.up * PJumpPower, ForceMode2D.Impulse);
+        PAni.SetBool("Player_Jump", true);
+        isGround = false;
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGround = true;
+            Debug.Log("점프 가능");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+
+        }
+    }
+
 }
