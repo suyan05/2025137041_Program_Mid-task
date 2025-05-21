91d5dd90b757fdbf40314ad1b59d4119a660d816
suyan05 <cadhppr588@gmail.com>
2025-05-21 16:19:46 +0900

    [fead]랭크 시스템

--- Assets/01.Scripts/UpdateGameManager.cs
+++ Assets/01.Scripts/UpdateGameManager.cs
@@ -6,11 +6,8 @@ using UnityEngine.UI;
 public class UpdateGameManager : MonoBehaviour
 {
     [Header("Rank")]
-    public GameObject Level_1_Panel;
-    public GameObject Level_2_Panel;
-    public GameObject Level_3_Panel;
-    public GameObject Level_4_Panel;
-    public GameObject Level_5_Panel;
+    public GameObject[] rankList;
+    public int rankStage = 0;
 
     public TMP_InputField inputField;
     public Button GameStartButton;
@@ -22,7 +19,37 @@ public class UpdateGameManager : MonoBehaviour
 
     private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            if (rankStage >= rankList.Length)
+            {
+                rankStage = 0;
+            }
+            else
+            rankStage++;
+        }
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            if (rankStage <= 0)
+            {
+                rankStage = 4;
+            }
+            else
+            rankStage--;
+        }
+
+        for (int i = 0; i < rankList.Length; i++)
+        {
+            if (i == rankStage)
+            {
+                rankList[i].SetActive(true);
+            }
+            else
+            {
+                rankList[i].SetActive(false);
+            }
+        }
     }
 
     private void OnGameStartButtonClicked()
